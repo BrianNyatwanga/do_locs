70bddbd83549a38dc8f7edaaf7c83d2ebeefad2d
BrianNyatwanga <briannyatwanga@gmail.com>
2020-07-18 23:08:27 -0400

    Navigation Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'home.dart';
 
-void main(){
-  runApp(
-    new MaterialApp(
-      home: Center(
-        child: Column(
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Homepage(),
+  ));
+}
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.red,
+        title: Text('DoLocs'),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          new IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {})
+        ],
+      ),
+      drawer: new Drawer(
+        child: new ListView(
           children: <Widget>[
-            Icon(Icons.person),
-            Text('user'),
-            Icon(Icons.person)
+            //header drawer
+            new UserAccountsDrawerHeader(
+                accountName: Text('Brian Nyatwanga'),
+                accountEmail: Text('briannyatwanga@gmail.com'),
+            currentAccountPicture: GestureDetector(
+              child: new CircleAvatar(
+                backgroundColor: Colors.grey,
+                child: Icon(Icons.person, color: Colors.white),
+              ),
+            ),
+              decoration: new BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+            // body
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Home Page'),
+                leading: Icon(Icons.home),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My Account'),
+                leading: Icon(Icons.person),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My Orders'),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Categories'),
+                leading: Icon(Icons.dashboard),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Favourites'),
+                leading: Icon(Icons.favorite),
+              ),
+            ),
+            Divider(),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(Icons.settings),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help),
+              ),
+            ),
           ],
         ),
       ),
-    )
-  );
+    );
+  }
 }
